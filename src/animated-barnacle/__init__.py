b8f0976e0a2c3f33069da8fac5577265a9698091
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-02-14 15:11:56 +0100

    Added project skeleton and init
    Closes #3

---
+++ src/animated-barnacle/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '0.1.0-dev'
+
+__title__ = 'animated-barnacle'
+__description__ = 'If Dr. Frankenstein were an arthropod, and also a python package'
+__url__ = 'https://github.com/cthoyt/animated-barnacle'
+
+__author__ = 'Charles Tapley Hoyt'
+__email__ = 'cthoyt@gmail.com'
+
+__license__ = 'Apache 2.0 License'
+__copyright__ = 'Copyright (c) 2017 Charles Tapley Hoyt'
