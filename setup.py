0d60168958139fa74e2ad3cbac651d6cd1b4eaeb
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-02-14 16:26:24 +0100

    Added setup.py
    Closes #5

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name='animated-barnacle',
+        version='0.1.0-dev',
+        author='Charles Tapley Hoyt',
+        author_email='cthoyt@gmail.com',
+        description='If Dr. Frankenstein were an arthropod, and also a python package',
+        license='Apache License 2.0',
+        url='https://github.com/cthoyt/animated-barnacle',
+
+        # Look for python code inside /src/
+        packages=setuptools.find_packages(where='src'),
+
+        # Assign the package-level folder ('') to be the one it finds in 'src'
+        package_dir={'': 'src'}
+    )
